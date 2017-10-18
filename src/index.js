7fe1aac39403fdca4f9fed240e60d7597d7fa629
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-10-18 21:10:03 +0200

    Set a clean initial state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <section className="main">
+      </section>
+    );
+  }
+}
+
+const app = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+console.log(app);
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
