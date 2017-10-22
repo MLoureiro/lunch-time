dc5524ba1b9c3ee891351013ebe16fb5b163fd62
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-10-22 16:16:52 +0200

    Make another awesome loader

--- src/components/Styled/Loader.js
+++ src/components/Styled/Loader.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Loader = styled.div`
+const Futuristic = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -48,4 +49,57 @@ const Loader = styled.div`
 }
 `;
 
-export { Loader };
+const BounceBall = styled.div`
+  width: 18px;
+  height: 18px;
+  background-color: #333;
+
+  border-radius: 100%;
+  display: inline-block;
+  -webkit-animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  animation: sk-bouncedelay 1.4s infinite ease-in-out both;
+  
+  :first-child {
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+  
+  :nth-child(2) {
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+  }
+
+  @-webkit-keyframes sk-bouncedelay {
+    0%, 80%, 100% { -webkit-transform: scale(0) }
+    40% { -webkit-transform: scale(1.0) }
+  }
+  
+  @keyframes sk-bouncedelay {
+    0%, 80%, 100% { 
+      -webkit-transform: scale(0);
+      transform: scale(0);
+    } 40% { 
+      -webkit-transform: scale(1.0);
+      transform: scale(1.0);
+    }
+  }
+`;
+
+const BouncerWrapper = styled.div`
+  margin: 100px auto 0;
+  width: 70px;
+  text-align: center;
+`;
+
+function Bouncer() {
+  return (
+    <BouncerWrapper>
+      <BounceBall />
+      <BounceBall />
+      <BounceBall />
+    </BouncerWrapper>
+  )
+}
+
+const Loader = Futuristic;
+export { Loader, Futuristic, Bouncer };
