2380cf231a7a9182b0f8a96ab1c4f84be623b19b
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-10-29 12:30:55 +0100

    Make Icon Buttons with the new style

---
+++ src/components/Styled/IconButton.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledIcon = styled.i.attrs({ 'aria-hidden': true })`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const StyledButton = styled.button`
+  font-size: ${({ theme }) => theme.button.font.size};
+
+  position: relative;
+  display: inline-flex;
+  justify-content: center;
+  overflow: hidden;
+
+  width: 3em;
+  height: 3em;
+  padding: 0;
+  border: none;
+  border-radius: 50%;
+
+  cursor: pointer;
+  user-select: none;
+  box-sizing: border-box;
+  box-shadow: 
+    0 3px 5px -1px rgba(0,0,0,.2), 
+    0 6px 10px 0 rgba(0,0,0,.14), 
+    0 1px 18px 0 rgba(0,0,0,.12);
+
+  background-color: ${({ theme }) => theme.button.color.primary.normal};
+  color: ${({ theme }) => theme.button.color.primary.text};
+
+  &:active {
+    background-color: ${({ theme }) => theme.button.color.primary.active};
+  }
+
+  &.secondary {
+    background-color: ${({ theme }) => theme.button.color.secondary.normal};
+    color: ${({ theme }) => theme.button.color.secondary.text};
+  
+    &:active {
+      background-color: ${({ theme }) => theme.button.color.secondary.active};
+    }
+  }
+
+  &.large {
+    width: 4em;
+    height: 4em;
+  }
+
+  &.small {
+    width: 2em;
+    height: 2em;
+  }
+
+  &.tiny {
+    font-size: .8em;
+    width: 1em;
+    height: 1em;
+  }
+`;
+
+function getSizeClassName({ large, small, tiny }) {
+  if (large) return 'large';
+  if (small) return 'small';
+  if (tiny) return 'tiny';
+  return '';
+}
+
+function IconButton({ icon, secondary, ...props }) {
+  return (
+    <StyledButton
+      className={`${secondary ? 'secondary' : ''} ${getSizeClassName(props)}`}
+      onClick={(event) => props.onClick(event)}
+      {...props}
+    >
+      <StyledIcon className={`fa fa-${icon}`} />
+    </StyledButton>
+  );
+}
+
+IconButton.propTypes = {
+  type: PropTypes.string,
+  onClick: PropTypes.func,
+  secondary: PropTypes.bool,
+  large: PropTypes.bool,
+  small: PropTypes.bool,
+  tiny: PropTypes.bool,
+};
+
+IconButton.defaultProps = {
+  type: 'button',
+  onClick: () => {},
+  secondary: false,
+  large: false,
+  small: false,
+  tiny: false,
+};
+
+export { IconButton };
--- src/theme.style.js
+++ src/theme.style.js
@@ -6,6 +6,19 @@ const colors = {
   iron: '#cccccc',
   gunPowder: '#474551',
   comet: '#636172',
+
+  primary: {
+    normal: '#90caf9',
+    light: '#c3fdff',
+    dark: '#5d99c6',
+    text: '#000000',
+  },
+  secondary: {
+    normal: '#455a64',
+    light: '#718792',
+    dark: '#1c313a',
+    text: '#ffffff',
+  },
 };
 
 const general = {
@@ -16,7 +29,43 @@ const general = {
   font: {
     family: 'Comfortaa, cursive',
     size: '16px',
-  }
+  },
+};
+
+const button = {
+  border: {
+    thickness: '1px',
+    radius: '3px',
+  },
+  color: {
+    primary: {
+      normal: colors.primary.normal,
+      active: colors.primary.dark,
+      text: colors.primary.text,
+    },
+    secondary: {
+      normal: colors.secondary.normal,
+      active: colors.secondary.light,
+      text: colors.secondary.text,
+    },
+    background: colors.white,
+    border: colors.iron,
+    text: colors.neptune,
+    hover: {
+      background: colors.gunPowder,
+      border: colors.gunPowder,
+      text: colors.white,
+    },
+    active: {
+      background: colors.comet,
+      border: colors.gunPowder,
+      text: colors.white,
+    },
+  },
+  font: {
+    size: general.font.size,
+    family: general.font.family,
+  },
 };
 
 const header = {
@@ -104,4 +153,4 @@ const modal = {
   },
 };
 
-export default { general, header, form, modal };
+export default { general, header, button, form, modal };
