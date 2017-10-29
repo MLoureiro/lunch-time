06dce1553a0156d36ddea1cb65351817eacc4d56
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-10-29 13:25:52 +0100

    Improve normal buttons

--- src/theme.style.js
+++ src/theme.style.js
@@ -94,49 +94,6 @@ const form = {
     padding: '.25em 1em',
     height: '1.5em',
   },
-  button: {
-    border: {
-      thickness: '1px',
-      radius: '3px',
-    },
-    color: {
-      background: colors.white,
-      border: colors.iron,
-      text: colors.neptune,
-      hover: {
-        background: colors.gunPowder,
-        border: colors.gunPowder,
-        text: colors.white,
-      },
-      active: {
-        background: colors.comet,
-        border: colors.gunPowder,
-        text: colors.white,
-      },
-    },
-    font: {
-      size: '1em',
-      weight: 'bold',
-      family: general.font.family,
-    },
-    size: {
-      large: {
-        padding: '1.5em 2em',
-        font: { size: '1.2em' },
-      },
-      small: {
-        padding: '.25em .5em',
-        font: { size: '1em' },
-      },
-      tiny: {
-        padding: '0 0.25em',
-        font: { size: '.8em' },
-      },
-    },
-    margin: '1em',
-    padding: '.5em 1em',
-    height: '1.5em',
-  },
 };
 
 const modal = {
