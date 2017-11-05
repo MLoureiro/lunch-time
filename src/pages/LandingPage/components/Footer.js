b9cad69f69933ff7a302d98317bf15ab285e2132
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-11-05 14:17:38 +0100

    Remove old logo image

--- src/pages/LandingPage/components/Footer.js
+++ src/pages/LandingPage/components/Footer.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const images = {
-  logo: require('../../../components/images/logo.svg')
-};
-
 const Wrapper = styled.section`
   display: flex;
   height: 40px;
