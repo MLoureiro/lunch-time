7584c06f57d8efaa1b0ab08dab0b1c7493e8e93e
Marcos Loureiro <mloureiro.dev@gmail.com>
2017-11-05 11:19:56 +0100

    Make initial Team repository

---
+++ src/services/TeamRepository.js
@@ -0,0 +1,32 @@
+import { app } from './rebase';
+
+class TeamRepository {
+  database;
+
+  constructor({ database }) {
+    this.database = database;
+  }
+
+  async add({ name }) {
+    this.database
+      .child(this._getNextId())
+      .set({
+        metadata: {
+          name,
+          createdAt: Date.now(),
+          // @todo add author as current user
+        },
+        members: [
+          // @todo add current user
+        ],
+      });
+  }
+
+  _getNextId() {
+    return this.database.push().key;
+  }
+}
+
+export default new TeamRepository({
+  database: app.database().ref('/teams/'),
+});
